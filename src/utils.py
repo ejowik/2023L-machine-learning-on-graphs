7565a940369fa52313a82c145473e7e575f3ba62
Szymon Hryniewski <hryniewskis@gmail.com>
2023-03-25 15:25:26 +0100

    CSL ensemble experiments, black isort refactor

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,18 @@
-import sys
-import os
 import json
+import os
+import pickle
+import sys
+import time
+from typing import List, Tuple
 
+import dgl
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import pandas as pd
-import networkx as nx
-
-import matplotlib.pyplot as plt
-from typing import Tuple, List
-
-from sklearn.model_selection import train_test_split, cross_val_score
+import torch
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, train_test_split
 
 
 def read_stargazers_dataset(path: str, verbose: bool = True) -> Tuple[List, pd.Series]:
@@ -53,3 +55,124 @@ def create_logreg_model(X, y, get_pred: bool = False, **kwargs):
     if get_pred:
         return model.predict_proba(X_test), y_test
     return (model.predict(X_test) == y_test).mean()
+
+
+class CSL(torch.utils.data.Dataset):
+    """
+    Circular Skip Link Graphs:
+    Source: https://github.com/PurdueMINDS/RelationalPooling/
+    """
+
+    def __init__(self, path="../datasets/CSL/"):
+        self.name = "CSL"
+        self.adj_list = pickle.load(
+            open(os.path.join(path, "graphs_Kary_Deterministic_Graphs.pkl"), "rb")
+        )
+        self.graph_labels = torch.load(
+            os.path.join(path, "y_Kary_Deterministic_Graphs.pt")
+        )
+        self.graph_lists = []
+
+        self.n_samples = len(self.graph_labels)
+        self.num_node_type = 1  # 41
+        self.num_edge_type = 1  # 164
+        self._prepare()
+
+    def _prepare(self):
+        t0 = time.time()
+        print("[I] Preparing Circular Skip Link Graphs v4 ...")
+        for sample in self.adj_list:
+            _g = dgl.from_scipy(sample)
+            g = dgl.remove_self_loop(_g)
+            g.ndata["feat"] = torch.zeros(g.number_of_nodes()).long()
+
+            # adding edge features as generic requirement
+            g.edata["feat"] = torch.zeros(g.number_of_edges()).long()
+
+            self.graph_lists.append(g)
+        self.num_node_type = self.graph_lists[0].ndata["feat"].size(0)
+        self.num_edge_type = self.graph_lists[0].edata["feat"].size(0)
+        print("[I] Finished preparation after {:.4f}s".format(time.time() - t0))
+
+    def __len__(self):
+        return self.n_samples
+
+    def __getitem__(self, idx):
+        return self.graph_lists[idx], self.graph_labels[idx]
+
+
+def weighted_mean(alpha: float):
+    return lambda x, y: alpha * x + (1 - alpha) * y
+
+
+def select_by_var(arr: np.array, alpha: float):
+    variances_ord = np.var(arr, axis=0).argsort()[::-1]
+    return arr[:, variances_ord][:, : int(alpha * arr.shape[1])]
+
+
+def by_weighted_variance(alpha: float):
+    return lambda x, y: np.concatenate(
+        (select_by_var(x, alpha), select_by_var(y, 1 - alpha)), axis=1
+    )
+
+
+def project_v_on_w(v, w):
+    """
+    here x and y are vectors not matrices!!!
+    """
+    return w * np.vdot(v, w) / np.linalg.norm(w, 2) ** 2
+
+
+def get_projected_concatenation(
+    vectors, keep_first: bool = True, proj_on_sum: bool = True
+):
+    """
+    again applicable if we're having vectors!!!
+    """
+    x, y = vectors
+    v = y if keep_first else x
+    proj_w = x + y if proj_on_sum else x if keep_first else y
+    return np.concatenate((x + y - v, project_v_on_w(v, proj_w)))
+
+
+def split(x: np.array):
+    half = int(x.size / 2)
+    return x[:half], x[half:]
+
+
+def one_by_projection(keep_skipgram: bool = True, proj_on_sum: bool = True):
+    return lambda x, y: np.apply_along_axis(
+        lambda z: get_projected_concatenation(split(z), keep_skipgram, proj_on_sum),
+        1,
+        np.concatenate((x, y), axis=1),
+    )
+
+
+def get_differences_of_projections(vectors):
+    x, y = vectors
+    return np.concatenate((x + y, y - project_v_on_w(y, x + y)))
+
+
+def projection_diff():
+    return lambda x, y: np.apply_along_axis(
+        lambda z: get_differences_of_projections(split(z)),
+        1,
+        np.concatenate((x, y), axis=1),
+    )
+
+
+def get_weighted_projections(vectors, alpha):
+    x, y = vectors
+    w_m = weighted_mean(alpha)
+    vecs_w_sum = w_m(x, y) * 2
+    return np.concatenate(
+        (project_v_on_w(x, vecs_w_sum), project_v_on_w(y, vecs_w_sum))
+    )
+
+
+def weighted_projections(alpha: float):
+    return lambda x, y: np.apply_along_axis(
+        lambda z: get_weighted_projections(split(z), alpha),
+        1,
+        np.concatenate((x, y), axis=1),
+    )
